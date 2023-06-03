59a2b6145fee8617aa4b41cd436cd13d1696b73d
cistercii <cistercii@mail.ru>
2023-06-04 00:56:06 +0300

    пуш ветки junit

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,6 +4,10 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException(
+                    "значение не должно быть отрицательным, а у вас : " + amount);
+        }
         return boundary - amount % boundary;
     }
 }
